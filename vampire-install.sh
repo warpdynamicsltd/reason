0d9d36a4af8fa16c2edd967e26655ac99bc71902
Michal Wojcik <michal.s.wojcik@gmail.com>
2025-06-20 18:02:31 +0200

    github ci

--- vampire-install.sh
+++ vampire-install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 cd repositories/vampire
-cmake .
+mkdir build
+cd build
+CC=/usr/bin/clang CXX=/usr/bin/clang++ cmake ..
 make -j4
 chmod 777 ./vampire
-mkdir -p ../../reason/assets/bin
-cp ./vampire ../../reason/assets/bin
+mkdir -p ../../../reason/assets/bin
+cp ./vampire ../../../reason/assets/bin
 
