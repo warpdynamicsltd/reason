9f65c87898155857df23618fbf75974f4ec7c3b2
Michal Stanislaw Wojcik <110554940+warpdynamicsltd@users.noreply.github.com>
2025-06-21 23:10:07 +0200

    close #12 (#13)

--- reason/parser/utils.py
+++ reason/parser/utils.py
@@ -4,8 +4,16 @@ from lark import v_args
 
 def v_args_return_with_meta(method):
     def modified_method(self, meta, children):
-        # print(meta.__dict__)
         res = method(self, *children)
+        if isinstance(res, reason.parser.tree.AbstractSyntaxTree):
+            setattr(res, "meta", meta)
+        return res
+
+    return v_args(meta=True)(modified_method)
+
+def v_args_with_meta_return_with_meta(method):
+    def modified_method(self, meta, children):
+        res = method(self, meta, *children)
         if isinstance(res, reason.parser.tree.AbstractSyntaxTree):
             setattr(res, "meta", meta)
         return res
