326bd27d64ff784aee90bb6adaaeed6a317bb72d
Michal <wojcik@warpdynamics.co.uk>
2025-07-02 21:17:50 +0200

    adds: function notation

--- reason/parser/__init__.py
+++ reason/parser/__init__.py
@@ -10,7 +10,12 @@ class Parser:
         with open(str(files("reason") / "assets" / "lark" / "reason.lark")) as f:
             self.dynamic_code = self.ogc.create_lark_code()
             code = f.read() + self.dynamic_code
+            self.save_code(code)
             self.reason_parser = get_lark_parser(code, start="logic_simple", lexer="basic")
+
+    def save_code(self, code):
+        with open(str(files("reason") / "assets" / "lark" / "reason_gen.lark"), "w") as f:
+            f.write(code)
 
     def __call__(self, text: str) -> AbstractSyntaxTree:
         tree = self.reason_parser.parse(text)
