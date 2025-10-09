9acb42dee13e627735a57aa36d73ebb987458b99
Michal Wojcik <wojcik@warpdynamics.co.uk>
2025-10-09 07:02:33 +0200

    fix tests and workflows: correct `add` function test assertion in `test_demo.py` and update workflow to install editable package

--- tests/cpp/test_demo.py
+++ tests/cpp/test_demo.py
@@ -5,5 +5,4 @@ from reason import cpp
 class TestCppTemplate(unittest.TestCase):
     def test_add_function(self):
         result = cpp.add(4, 7)
-        self.assertEqual(result, 12)
-        print(result)
+        self.assertEqual(result, 11)
