3c41dd62842cb5559d3d3dc8618a5eb53a8dc423
Michal Wojcik <wojcik@warpdynamics.co.uk>
2025-10-09 06:34:44 +0200

    add support for C++ extension: configure `reason.cpp` module with `add` function, update build system to `setuptools`, require Python 3.13, and include unit test

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
-import reason
-from setuptools import setup, find_packages
+#import reason
+from setuptools import setup, find_packages, Extension
 
 
 setup(
     name="reason",
     version="1.0.0",
     package_data={"reason": ["assets/lark/reason.lark", "assets/lark/tptp.lark", "assets/bin/vampire"]},
-    packages=find_packages()
+    packages=find_packages(),
+    ext_modules=[
+            Extension(
+                "reason.cpp",
+                ["reason/cpp/demo.cpp"],
+                language="c++",
+            ),
+    ]
 )
---
+++ tests/cpp/test_demo.py
@@ -0,0 +1,9 @@
+import unittest
+
+from reason import cpp
+
+class TestCppTemplate(unittest.TestCase):
+    def test_add_function(self):
+        result = cpp.add(4, 7)
+        self.assertEqual(result, 12)
+        print(result)
