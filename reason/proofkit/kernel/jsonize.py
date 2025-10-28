fb5f1403f17e8bd786ec6256759ffbd6a9b4337e
Michal Wojcik <wojcik@warpdynamics.co.uk>
2025-10-28 20:37:25 +0100

    refine kernel json processing: update `SkolemConst` serialization by replacing redundant `name` field with `args` list for improved consistency and clarity in JSON schema

--- reason/proofkit/kernel/jsonize.py
+++ reason/proofkit/kernel/jsonize.py
@@ -7,7 +7,7 @@ class ProofKitJSONizer(JSONizer):
             return dict(type="ContextConst", name=int(name[8:]))
         if name[:7] == "skolem_":
             lst = list(map(int, name[7:].split("_")))
-            return dict(type="SkolemConst", name="", args=lst)
+            return dict(type="SkolemConst", name=lst)
         return dict(type="Const", name=name)
 
 def jsonize(formula: FirstOrderFormula):
